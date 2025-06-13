7d805d5238bca2319daad0103e65950331679b6b
gaharoe <chndr3303@gmail.com>
2025-06-13 11:00:15 +0700

    setup

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
-
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-	res.sendFile(__dirname+"/public/index.html");
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/burp-test", (req, res) => {
@@ -14,6 +14,10 @@ app.post("/burp-test", (req, res) => {
 	res.status(200);
 	res.send({data: "bla bla bla", id: "021"});
 	console.log(req.body);
+});
+
+app.get("/login", (req, res) => {
+
 });
 
 app.listen(80, () => {

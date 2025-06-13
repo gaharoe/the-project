9258dc824710dc4f1606f6e333eb22830dda071d
gaharoe <chndr3303@gmail.com>
2025-06-13 10:31:49 +0700

    first

---
+++ ctf-solves.md
@@ -0,0 +1,5 @@
+EASY ====================================================
+verify: picoCTF{trust_but_verify_451fd69b}
+scan-surprise: picoCTF{p33k_@_b00_a81f0a35}
+Fantasy CTF: picoCTF{m1113n1um_3d1710n_fc649bc5}
+=========================================================
---
+++ server.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname+"/public/index.html");
+});
+
+app.post("/burp-test", (req, res) => {
+	console.log("request done");
+	res.status(200);
+	res.send({data: "bla bla bla", id: "021"});
+	console.log(req.body);
+});
+
+app.listen(80, () => {
+	console.log("server running");
+});
